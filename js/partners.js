693de01524e2eb6d72223ca3a070eb86a297c7ad
Anthony8845 <46643219+Anthony8845@users.noreply.github.com>
2021-11-29 20:13:43 +0300

    update partners

--- js/partners.js
+++ js/partners.js
@@ -9,7 +9,7 @@ const partners = () => {
     data.forEach((item) => {
       const { image, kitchen, name, price, products, stars, time_of_delivery } = item
       const a = document.createElement('a')
-      a.setAttribute('href', '/restaurant.html')
+      a.setAttribute('href', 'restaurant.html')
       a.classList.add('card')
       a.classList.add('card-restaurant')
 
@@ -41,7 +41,7 @@ const partners = () => {
 
         localStorage.setItem('restaurant', JSON.stringify(item))
 
-        window.location.href = '/restaurant.html'
+        window.location.href = 'restaurant.html'
       })
       cardsRestaurants.append(a)
     });
